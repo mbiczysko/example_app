dfea6e0e4b3fa98fd11195b23aea79e320ad1cc7
Marek Biczysko <marek.biczysko@teamaol.com>
2017-07-19 14:33:10 +0100

    Add Calculator class to that can do mathematical differentiation of polynomials

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,47 @@
 module App
+end
 
-  def self.foo
-    'bar'
-  end
+class Calculator
 
-  class Person
-    def exist?
-      true
-    end
+  def run(array_of_number)
+      equation = ""
+
+      array_of_number.each_with_index { |number, index|
+        power = array_of_number.count - 1 - index
+        first_part = (number*power)
+        second_part = (power-1)
+
+        l_equation = ""
+        case first_part
+        when 0
+          l_equation += ""
+        else
+         l_equation += first_part.to_s
+        end
+
+        case second_part
+        when -1
+          l_equation += ""
+        when 0
+          l_equation += ""
+        when 1
+         l_equation += "x"
+        else
+         l_equation += "x^" + second_part.to_s
+        end
+
+        if equation != ""  && l_equation != ""
+          case l_equation.include?("-")
+          when true
+            l_equation  = l_equation
+          else
+            l_equation  = '+' + l_equation
+          end
+        end
+        equation += l_equation
+      }
+
+      return equation
   end
 end
 
--- spec/example_app_spec.rb
+++ spec/example_app_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 require 'app'
 
-describe App, '#foo' do
-  it "#foo" do
-    expect(App.foo).to eq('bar')
-  end
+describe Calculator, '#run' do
+  let(:calculator) {Calculator.new}
 
-  let(:person) {App::Person.new}
-
-  it "Person" do
-    expect(person).to exist
+  it "#run" do
+    expect(calculator.run([0])).to eql('')
+    expect(calculator.run([1,0])).to eql('1')
+    expect(calculator.run([1,0,0])).to eql('2x')
+    expect(calculator.run([3,2,1])).to eql('6x+2')
+    expect(calculator.run([4,3,0,1])).to eql('12x^2+6x')
+    expect(calculator.run([4,-5,0,1])).to eql('12x^2-10x')
   end
 
 end
+
